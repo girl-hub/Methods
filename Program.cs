38b41649c05d912d662b9a26c232499f9fbb7fa1
Yuliia Dolinina <julia.dolinina@gmail.com>
2022-10-16 16:23:01 -0400

    my project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Return_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the name of your venue?");
+            string venue = args[0];
+            Announce(venue);
+
+        }
+
+        static void Announce(string venue)
+        {
+            Console.WriteLine(venue + "will have band performing tonight!");
+        }
+    }
+}
+
+        
+    
+
